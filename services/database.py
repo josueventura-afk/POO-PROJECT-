88adf9d782be288174878a56866f8ca716e39d16
Anne-w007 <anel.lobo@uab.edu.bo>
2026-06-08 17:07:06 -0400

    Configuracion conexion local SQL Server

--- services/database.py
+++ services/database.py
@@ -5,24 +5,17 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 
 def get_connection_string() -> str:
-    env_connection = os.getenv("SQL_SERVER_CONNECTION_STRING")
-    if env_connection:
-        return env_connection
-
-    user = os.getenv("SQL_SERVER_USER", "sa")
-    password = os.getenv("SQL_SERVER_PASSWORD", "YourPassword123")
-    server = os.getenv("SQL_SERVER_HOST", "localhost\\SQLEXPRESS")
-    database = os.getenv("SQL_SERVER_DATABASE", "POO_Proyecto")
-    driver = os.getenv("SQL_SERVER_DRIVER", "ODBC Driver 18 for SQL Server")
-
     connection_url = URL.create(
         "mssql+pyodbc",
-        username=user,
-        password=password,
-        host=server,
-        database=database,
-        query={"driver": driver, "TrustServerCertificate": "yes"},
+        host="localhost\\SQLEXPRESS",
+        database="POO_Proyecto",
+        query={
+            "driver": "ODBC Driver 18 for SQL Server",
+            "trusted_connection": "yes",
+            "TrustServerCertificate": "yes",
+        },
     )
+
     return str(connection_url)
 
 
